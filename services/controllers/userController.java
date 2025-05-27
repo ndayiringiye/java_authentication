1fe29d9a04c9bad7b9bc888e1045e83dea2e7b08
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-05-27 17:39:00 +0200

    feat:/ register fixed successfully

--- services/controllers/userController.java
+++ services/controllers/userController.java
@@ -1,15 +1,9 @@
-@RestController
-public class UserController {
-	@Autowired
-	private UserService userService;
+@PostMapping("/register")
+public ResponseEntity<?> register(@ModelAttribute User user) {
+	return ResponseEntity.ok(userService.register(user));
+}
 
-	@PostMapping("/register")
-	public ResponseEntity<?> register(@RequestBody User user) {
-		return ResponseEntity.ok(userService.register(user));
-	}
-
-	@PostMapping("/login")
-	public ResponseEntity<?> login(@RequestBody User user) {
-		return ResponseEntity.ok(userService.login(user.getEmail(), user.getPassword()));
-	}
+@PostMapping("/login")
+public ResponseEntity<?> login(@ModelAttribute User user) {
+	return ResponseEntity.ok(userService.login(user.getEmail(), user.getPassword()));
 }
--- services/userServices.java
+++ services/userServices.java
@@ -4,8 +4,14 @@ public class UserService {
 	private UserRepository userRepository;
 
 	public User register(User user) {
+		return userRepository.save(user);
 	}
 
 	public User login(String email, String password) {
+		User user = userRepository.findByEmail(email);
+		if (user != null && user.getPassword().equals(password)) {
+			return user;
+		}
+		throw new RuntimeException("Invalid email or password");
 	}
 }
