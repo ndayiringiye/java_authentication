3929821713c663863b0da161178482148d8bfdf9
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-05-27 17:26:52 +0200

    feat:/user services in java provided successfully

---
+++ services/userServices.java
@@ -0,0 +1,11 @@
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+
+	public User register(User user) {
+	}
+
+	public User login(String email, String password) {
+	}
+}
